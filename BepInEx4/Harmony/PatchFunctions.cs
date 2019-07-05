9b73f66aaf41b1486fdad1e5b69685e909356b1a
ghorsington <neigh@coder.horse>
2019-07-05 10:35:45 +0300

    Use Harmony 2 to perform patching

--- BepInEx4/Harmony/PatchFunctions.cs
+++ BepInEx4/Harmony/PatchFunctions.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using HarmonyLib;
-using Memory = Harmony.ILCopying.Memory;
+using ShimHelpers;
 
 namespace Harmony
 {
     public static class PatchFunctions
     {
+        private static readonly Type patchFunctionsNew =
+            typeof(HarmonyLib.Harmony).Assembly.GetType("HarmonyLib.PatchFunctions");
+
+        public static Action<MethodBase, PatchInfo, string> UpdateWrapper =
+            ShimUtil.MakeDelegate<Action<MethodBase, PatchInfo, string>>(patchFunctionsNew, nameof(UpdateWrapper));
+
+
         public static void AddPrefix(PatchInfo patchInfo, string owner, HarmonyMethod info)
         {
-            if (info == null || info.method == null) return;
+            if (info?.method == null) return;
             var priority = info.prioritiy == -1 ? 400 : info.prioritiy;
             var before = info.before ?? new string[0];
             var after = info.after ?? new string[0];
@@ -25,7 +30,7 @@ namespace Harmony
 
         public static void AddPostfix(PatchInfo patchInfo, string owner, HarmonyMethod info)
         {
-            if (info == null || info.method == null) return;
+            if (info?.method == null) return;
             var priority = info.prioritiy == -1 ? 400 : info.prioritiy;
             var before = info.before ?? new string[0];
             var after = info.after ?? new string[0];
@@ -39,7 +44,7 @@ namespace Harmony
 
         public static void AddTranspiler(PatchInfo patchInfo, string owner, HarmonyMethod info)
         {
-            if (info == null || info.method == null) return;
+            if (info?.method == null) return;
             var priority = info.prioritiy == -1 ? 400 : info.prioritiy;
             var before = info.before ?? new string[0];
             var after = info.after ?? new string[0];
@@ -54,29 +59,6 @@ namespace Harmony
         public static void RemovePatch(PatchInfo patchInfo, MethodInfo patch)
         {
             patchInfo.RemovePatch(patch);
-        }
-
-        public static List<MethodInfo> GetSortedPatchMethods(MethodBase original, Patch[] patches)
-        {
-            return (from p in patches
-                where p.patch != null
-                orderby p
-                select p.GetMethod(original)).ToList();
-        }
-
-        public static void UpdateWrapper(MethodBase original, PatchInfo patchInfo, string instanceID)
-        {
-            var sortedPatchMethods = GetSortedPatchMethods(original, patchInfo.prefixes);
-            var sortedPatchMethods2 = GetSortedPatchMethods(original, patchInfo.postfixes);
-            var sortedPatchMethods3 = GetSortedPatchMethods(original, patchInfo.transpilers);
-            var dynamicMethod = MethodPatcher.CreatePatchedMethod(original, instanceID, sortedPatchMethods,
-                sortedPatchMethods2, sortedPatchMethods3);
-            if (dynamicMethod == null)
-                throw new MissingMethodException("Cannot create dynamic replacement for " + original.FullDescription());
-            var methodStart = Memory.GetMethodStart(original);
-            var methodStart2 = Memory.GetMethodStart(dynamicMethod);
-            Memory.WriteJump(methodStart, methodStart2);
-            PatchTools.RememberObject(original, dynamicMethod);
         }
     }
 }
